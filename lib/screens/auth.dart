3442d5c49a3e312cc87aa548dbb292b682921395
yogesh <yogeshyergalwar@gmail.com>
2025-04-08 18:01:32 +0530

    initial commit

---
+++ lib/screens/auth.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+
+class authScreen extends StatefulWidget {
+  const authScreen({super.key});
+
+  @override
+  State<authScreen> createState() => _authScreenState();
+}
+
+class _authScreenState extends State<authScreen> {
+  var _isLogin = true;
+  final _Form = GlobalKey<FormState>();
+  var Enteremsil = '';
+  var Enterpassword = '';
+
+  void _submit() {
+    final isValid = _Form.currentState!.validate();
+    if (isValid) {
+      _Form.currentState!.save();
+      print(Enterpassword);
+      print(Enteremsil);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.primary,
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                margin:
+                    EdgeInsets.only(top: 30, left: 20, right: 20, bottom: 20),
+                width: 200,
+                child: Image.asset('assets/images/chat.png'),
+              ),
+              Card(
+                margin: EdgeInsets.all(20),
+                child: SingleChildScrollView(
+                  padding: EdgeInsets.all(15),
+                  child: Form(
+                    key: _Form,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        TextFormField(
+                          decoration: InputDecoration(
+                            label: Text('Email Adress'),
+                          ),
+                          keyboardType: TextInputType.emailAddress,
+                          textCapitalization: TextCapitalization.none,
+                          autocorrect: false,
+                          validator: (value) {
+                            if (value == null ||
+                                value.trim().isEmpty ||
+                                !value.contains('@'))
+                              return 'Please Enter Valid Email Id';
+                            return null;
+                          },
+                          onSaved: (value){
+                            Enteremsil=value!;
+                          },
+                        ),
+                        TextFormField(
+                          decoration: InputDecoration(
+                            label: Text('Password'),
+                          ),
+                          textCapitalization: TextCapitalization.none,
+                          autocorrect: false,
+                          obscureText: true,
+                          validator: (value) {
+                            if (value == null || value.trim().length <= 6)
+                              return 'Password Contains atleast more than 6 characters';
+                            return null;
+                          },
+                          onSaved: (value){
+                            Enterpassword=value!;
+                          },
+                        ),
+                        ElevatedButton(
+                            onPressed: _submit,
+                            child: Text(_isLogin ? 'Login' : 'Signup')),
+                        TextButton(
+                            onPressed: () {
+                              setState(() {
+                                _isLogin = _isLogin ? false : true;
+                              });
+                            },
+                            child: Text(_isLogin
+                                ? 'Create an account'
+                                : 'I aready have an account'))
+                      ],
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
