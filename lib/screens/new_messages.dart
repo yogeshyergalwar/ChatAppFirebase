789bfda0c7c4e3381d0f462127da4cc2270b30f2
yogesh <yogeshyergalwar@gmail.com>
2025-04-18 10:46:01 +0530

    pushNotifications added on every mesg sent

--- lib/screens/new_messages.dart
+++ lib/screens/new_messages.dart
@@ -1,4 +1,4 @@
-import 'package:chat_app/screens/pishnotification.dart';
+import 'package:chat_app/screens/pushnotification.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
